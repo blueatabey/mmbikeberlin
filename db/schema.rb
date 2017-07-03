83150c0a4aa89074c952613333241582b1807f23
Mariana <blue.atabey@gmail.com>
2017-07-03 14:03:29 -0400

    4.2 add/rename color in product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614151245) do
+ActiveRecord::Schema.define(version: 20170630161056) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170614151245) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "colour"
   end
 
 end
