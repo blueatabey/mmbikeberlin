0260eb3fcd0a8339680ba90f3ed5f390fa74bf0a
Mariana <blue.atabey@gmail.com>
2017-07-03 20:58:30 -0400

    4.2 db migrate

---
+++ db/migrate/20170704005305_image_fix.rb
@@ -0,0 +1,4 @@
+class ImageFix < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630161056) do
+ActiveRecord::Schema.define(version: 20170704005305) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
