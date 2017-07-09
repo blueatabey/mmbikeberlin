27fa1bd093a5f7f1df95a055966dd0e1c816b095
Mariana <blue.atabey@gmail.com>
2017-07-09 07:16:21 -0400

    4.3 add controllers & landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   resources :products
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
+
+  root 'static_pages#landing_page'
+
   root 'static_pages#index'
+
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
