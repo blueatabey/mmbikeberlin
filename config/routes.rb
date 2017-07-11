de9c597187fba5e3d18c66b4d4343fa0dc2fc396
Mariana <blue.atabey@gmail.com>
2017-07-11 13:49:53 -0400

    5.1 Updated site root, added route change to new product, updated nav links to dynamic

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  root 'static_pages#landing_page'
-
-  root 'static_pages#index'
+  root 'static_pages#contact'  
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
