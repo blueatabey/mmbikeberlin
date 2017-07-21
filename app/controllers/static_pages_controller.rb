dfbbfcf5c6873c82a76b2b49b528b943d63bf593
Mariana <blue.atabey@gmail.com>
2017-07-21 13:11:15 -0400

    5.3 contact form, thank you, email setup

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,16 @@ class StaticPagesController < ApplicationController
 
   def landing_page
   	@products = Product.limit(3)
+  end
+
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(from: @email,
+  		to: 'your-email@example.com',
+  		subject: "A new contact form message from #{@name}",
+  		body: @message).deliver_now
   end	
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  root 'static_pages#landing_page'  
+  root 'static_pages#landing_page' 
+
+  post 'static_pages/thank_you' 
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
-
