40f32c761341658ed33e13636cf01175612aa0db
Mariana <blue.atabey@gmail.com>
2017-08-18 20:02:59 -0400

    5.8 add new comment, fix show on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :products do
+    resources :comments
+  end  
   resources :users
-  resources :products
 
   get 'static_pages/about'
 
